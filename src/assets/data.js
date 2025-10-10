d2e14a3c041d689779fb78eab8c733451d3e3857
Adeyemo Taiwo M <adeyemotaiwom999@gmail>
2025-10-10 12:58:46 +0100

    update price

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,35 +1,35 @@
 export const productData = [
   {
     title: "Samosa",
-    price: "200",
+    price: "300",
     description:
       "Crispy golden rolls filled with a tasty mix of vegetables and spices, perfect for light snacking",
     imageName: "/product-image-1.png",
   },
   {
     title: "Sausage Roll",
-    price: "200",
+    price: "800",
     description:
       "Flaky pastry wrapped around a juicy, seasoned sausage, baked to perfection for a hearty treat.",
     imageName: "/product-image-4.png",
   },
   {
     title: "Spring Rolls",
-    price: "200",
+    price: "300",
     description:
       "Soft, fluffy, and slightly sweet fried dough balls, a crowd favorite at any event.",
     imageName: "/product-image-2.png",
   },
   {
     title: "Puff Puff",
-    price: "200",
+    price: "100",
     description:
       "Soft, fluffy, and slightly sweet fried dough balls, a crowd favorite at any event.",
     imageName: "/product-image-3.png",
   },
   {
     title: "Sausage Roll",
-    price: "200",
+    price: "800",
     description:
       "Flaky pastry wrapped around a juicy, seasoned sausage, baked to perfection for a hearty treat.",
     imageName: "/product-image-4.png",
@@ -37,7 +37,7 @@ export const productData = [
 
   {
     title: "Spring Rolls",
-    price: "200",
+    price: "300",
     description:
       "Soft, fluffy, and slightly sweet fried dough balls, a crowd favorite at any event.",
     imageName: "/product-image-2.png",
